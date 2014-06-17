a076ead35d0765af7749b3191f4a83d214f9b6f3
Emily Kelton <emilyk000@gmail.com>
2014-06-16 20:54:37 -0700

    added keydown function

--- js/app.js
+++ js/app.js
@@ -25,7 +25,22 @@ $(document).ready(function() {
 		$('.ryu-throwing').hide();
   	$('.ryu-ready').show();
   });
+  $(document).keydown(function() {
+    if ( event.which == 88 ) {
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').show();
+    }  
+  });
+  $(document).keyup(function() {
+    if ( event.which == 88 ) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  })
 });
+
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
